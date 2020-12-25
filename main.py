6a57a698625e8a5092e8e78e2176fc98177665ff
ltang23 <ltang23@illinois.edu>
2020-12-25 12:40:23 -0600

    added excpetion handling

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from WebScraper import WebScraper
 import time
 from datetime import datetime
 import logging 
+import sys
+
 
 logging.basicConfig(filename="logs.log", filemode="w", level=logging.INFO)
 logger = logging.getLogger("where-is-my-ak")
@@ -28,64 +30,66 @@ def set_viewport_size(driver, width, height):
 
 
 def main():
-
-    os.environ['webdriver.chrome.driver'] = './chromedriver'
-    twilio = TwilioClient()
-    items = []
-    
-    while (True):
-        #browser = webdriver.Chrome(executable_path="./chromedriver")
-        browser = webdriver.Firefox()
-        browser.set_window_size(1600, 900) # fu*k reCAPTCHA
-        #logger.info(browser.execute_script("return [window.innerWidth, window.innerHeight];"))
-        browser.get(url)
+    try:
+        os.environ['webdriver.chrome.driver'] = './chromedriver'
+        twilio = TwilioClient()
+        items = []
         
-        soup = BeautifulSoup(browser.execute_script("return document.body.innerHTML"), "lxml")
-        
-        #os.system("pkill chromedriver")
-        #os.system("pkill chrome")
-        os.system("pkill firefox")
-        os.system("pkill geckodriver")
-
-        stock = soup.findAll("li", {"class": "item product product-item"})
-        logger.info('---------------------------------------------------------')
-        logger.info("Number of items=" + str(len(stock)))
-        msg  = "In Stock Now!\n"
-        is_anything_in_stock = False
-        current_items = []
-        for item in stock:
-            item_soup = BeautifulSoup(str(item), "lxml")
-            item_name = item_soup.find("a", {"class" : "product-item-link"}).text.replace("\n", "").replace("\t","")
-            #print(item_name)
+        while (True):
+            #browser = webdriver.Chrome(executable_path="./chromedriver")
+            browser = webdriver.Firefox()
+            browser.set_window_size(1600, 900) # fu*k reCAPTCHA
+            #logger.info(browser.execute_script("return [window.innerWidth, window.innerHeight];"))
+            browser.get(url)
             
-            in_stock = item_soup.find("button", {"title" : "Add to Cart"})
-            if in_stock is not None:  # in stock
-                msg += item_name[0:50] + "\n"
-                is_anything_in_stock = True
-                current_items.append(item_name)
-        
-        change = False
-        for i in current_items:
-            if i not in items:
-                change = True
-                items = current_items
+            soup = BeautifulSoup(browser.execute_script("return document.body.innerHTML"), "lxml")
+            
+            #os.system("pkill chromedriver")
+            #os.system("pkill chrome")
+            os.system("pkill firefox")
+            os.system("pkill geckodriver")
 
-        if is_anything_in_stock is True and change is False:
-            logger.info("items in stock but no change from last check.")
-        notify = is_anything_in_stock and change
+            stock = soup.findAll("li", {"class": "item product product-item"})
+            logger.info('---------------------------------------------------------')
+            logger.info("Number of items=" + str(len(stock)))
+            msg  = "In Stock Now!\n"
+            is_anything_in_stock = False
+            current_items = []
+            for item in stock:
+                item_soup = BeautifulSoup(str(item), "lxml")
+                item_name = item_soup.find("a", {"class" : "product-item-link"}).text.replace("\n", "").replace("\t","")
+                #print(item_name)
+                
+                in_stock = item_soup.find("button", {"title" : "Add to Cart"})
+                if in_stock is not None:  # in stock
+                    msg += item_name[0:50] + "\n"
+                    is_anything_in_stock = True
+                    current_items.append(item_name)
+            
+            change = False
+            for i in current_items:
+                if i not in items:
+                    change = True
+                    items = current_items
 
-        now = datetime.now().strftime("%H:%M:%S")
-        if notify:  
-            logger.info(now + " " + msg)
-            twilio.sendMessage(msg)
-            #cmd = "echo '"+msg +"'| mail -s 'Get the fuck onto PSA and buy shit' larrtang@gmail.com"
-            #print(msg)
-            #os.system(cmd)
-            time.sleep(60*10)
-        else:               
-            logger.info(now + " Nothing found, sleeping")
-            time.sleep(60)
+            if is_anything_in_stock is True and change is False:
+                logger.info("items in stock but no change from last check.")
+            notify = is_anything_in_stock and change
 
+            now = datetime.now().strftime("%H:%M:%S")
+            if notify:  
+                logger.info(now + " " + msg)
+                twilio.sendMessage(msg)
+                #cmd = "echo '"+msg +"'| mail -s 'Get the fuck onto PSA and buy shit' larrtang@gmail.com"
+                #print(msg)
+                #os.system(cmd)
+                time.sleep(60*10)
+            else:               
+                logger.info(now + " Nothing found, sleeping")
+                time.sleep(60)
+    except Exception e:
+        logger.fatal(str(e))
+        sys.exit(-1)
 
 if __name__ == "__main__":
     main()
